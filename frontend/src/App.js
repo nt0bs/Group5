19c208653c8a085e6c0a84816e054de88d310c6f
CynthiaMokasi <116590682+CynthiaMokasi@users.noreply.github.com>
2024-10-02 00:57:46 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import Listjobs from './components/Listjobs'; // Ensure this path is correct
-import './App.css'; 
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/Homepage";
+import AddJob from "./components/Addjob";
+import JobDetails from "./components/Jobdetails";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { ListJobs } from "./components/Listjobs";
+import Navbar from "./components/Navbar";
 
+const App = () => {
+  const [jobs, setJobs] = useState([]);
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Job Listing Platform</h1>
-            <Listjobs />
-        </div>
-    );
-}
+  const addJob = (job) => {
+    setJobs([...jobs, { id: jobs.length + 1, ...job }]);
+  };
+
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/list-jobs"
+          element={<ListJobs jobs={jobs} />}
+        />
+        <Route
+          path="/add-job"
+          element={<AddJob addJob={addJob} />}
+        />
+        <Route
+          path="/jobs/:id"
+          element={<JobDetails jobs={jobs} />}
+        />
+        <Route
+          path="*"
+          element={<HomePage jobs={jobs} />}
+        />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
