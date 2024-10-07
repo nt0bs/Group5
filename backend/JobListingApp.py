9355a546188c6f73e4cfc6fa53d58fb2cfb55e2d
Khanyisile Buthelezi <thandofatimabuthelezi@gmail.com>
2024-10-07 15:59:41 +0200

    Added App configuration for Render Deployment

--- backend/JobListingApp.py
+++ backend/JobListingApp.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -20,5 +21,6 @@ with app.app_context():
 app.register_blueprint(jobListingApp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
